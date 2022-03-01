4a72b9be0b200a4233608494461350fa6de612d0
Patrick Charles-Lundaahl <patrick.charles-lundaahl@boldcommerce.com>
2022-03-01 10:02:55 -0800

    strengthen types in braintree-graph-api

--- src/services/api/HttpRequestInterface.ts
+++
@@ -1,9 +0,0 @@
-export interface HttpRequest {
-    host: string;
-    token: string;
-    headers: {
-        Accept: string;
-        Authorization: string;
-        [key: string]: string;
-    }
-}
