0ba5a3d9c914fe64458f468d3d00fa9e6917c41d
Patrick Charles-Lundaahl <patrick.charles-lundaahl@boldcommerce.com>
2022-03-01 09:27:01 -0800

    boldv2.ts: fix linting issues, switch to using axios instance

--- src/services/api/boldv2.ts
+++ src/services/api/boldv2.ts
@@ -1,67 +1,66 @@
-import axios, { AxiosResponse } from 'axios';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import axios, { AxiosResponse, AxiosInstance } from 'axios';
 import * as dotenv from 'dotenv';
-
-import { HttpRequest } from './HttpRequestInterface';
 
 dotenv.config();
 
+interface ErrorObject {
+    error: unknown,
+    status: number,
+}
 
-const formatAxiosResponse = <T>(promise: Promise<AxiosResponse<T>>) => promise
-    .then(({ data, status }) => ({ data, status }))
+const formatAxiosResponse = <T>(promise: Promise<AxiosResponse<T>>): Promise<AxiosResponse<T> | ErrorObject> => promise
     .catch((error: any) => ({
         error: error.response.data,
         status: error.response.status as number,
     }));
 
-
 class Boldv2 {
-    host: HttpRequest['host'];
-
-    token: HttpRequest['token'];
-
-    headers: HttpRequest['headers'];
+    private axios: AxiosInstance;
 
     constructor() {
-        this.host = 'https://api.boldcommerce.com';
-        const boldDemoApi = process.env.BOLD_ACCESS_TOKEN;
-        if (boldDemoApi === undefined) {
+        const token = process.env.BOLD_ACCESS_TOKEN;
+        if (token === undefined) {
             throw new Error('Bold Demo Api is undefined');
         }
-        this.token = boldDemoApi;
-        this.headers = {
-            'Content-Type': 'application/json',
-            Accept: 'application/json',
-            Authorization: `Bearer ${this.token}`,
-        };
+
+        this.axios = axios.create({
+            baseURL: 'https://api.boldcommerce.com',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+        });
     }
 
     async get(params: string) {
         return formatAxiosResponse(
-            axios.get(`${this.host}${params}`, { headers: this.headers }),
+            this.axios.get(params),
         );
     }
 
     async delete(params: string) {
         return formatAxiosResponse(
-            axios.delete(`${this.host}${params}`, { headers: this.headers }),
+            this.axios.delete(params),
         );
     }
 
     async post(params: string, body: any) {
         return formatAxiosResponse(
-            axios.post(`${this.host}${params}`, body, { headers: this.headers }),
+            this.axios.post(params, body),
         );
     }
 
     async put(params: string, body: any) {
         return formatAxiosResponse(
-            axios.put(`${this.host}${params}`, body, { headers: this.headers }),
+            this.axios.put(params, body),
         );
     }
 
     async patch(params: string, body: any) {
         return formatAxiosResponse(
-            axios.patch(`${this.host}${params}`, body, { headers: this.headers }),
+            this.axios.patch(params, body),
         );
     }
 }
